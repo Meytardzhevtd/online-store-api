ad31c52e1654923f3f6e46bf3ac3e66114ba6467
Meytardzhevtd <meytardzhevtd@yandex.ru>
2026-03-01 21:58:20 +0300

    create Prodact logic (begin)

---
+++ src/main/java/com/online/store/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.online.store.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.online.store.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findById(Long id);
+}
