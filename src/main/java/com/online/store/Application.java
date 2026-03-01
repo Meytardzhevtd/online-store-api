31745e773509d7d55988bde86f04e681c7505821
Meytardzhevtd <meytardzhevtd@yandex.ru>
2026-03-01 21:03:26 +0300

    Init repository

---
+++ src/main/java/com/online/store/Application.java
@@ -0,0 +1,19 @@
+package com.online.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, Online Store API!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
