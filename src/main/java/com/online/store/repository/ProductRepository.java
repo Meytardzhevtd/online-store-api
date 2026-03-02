a2848bea075df81aa434a7fc8229b41cec3ced0f
Meytardzhevtd <meytardzhevtd@yandex.ru>
2026-03-02 14:37:39 +0300

    fix: add @Repository in repository/

--- src/main/java/com/online/store/repository/ProductRepository.java
+++ src/main/java/com/online/store/repository/ProductRepository.java
@@ -3,9 +3,11 @@ package com.online.store.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.online.store.entity.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findById(Long id);
 }
