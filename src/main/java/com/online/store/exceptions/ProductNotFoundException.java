b7089bed0fa2d3506197c8c154d04efb74acc612
Timofey Meytardzhev <meytardzhevtd@yandex.ru>
2026-03-06 12:54:12 +0300

    add exceptions

---
+++ src/main/java/com/online/store/exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.online.store.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(Long id) {
+        super("Product with id = " + id + " not foound");
+    }
+}
