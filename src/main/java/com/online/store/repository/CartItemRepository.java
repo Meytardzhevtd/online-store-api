afa235b625c49a4ece814c5a102733507441a016
Timofey Meytardzhev <meytardzhevtd@yandex.ru>
2026-03-07 17:52:08 +0300

    fix: CartItemRepository

--- src/main/java/com/online/store/repository/CartItemRepository.java
+++ src/main/java/com/online/store/repository/CartItemRepository.java
@@ -1,12 +1,18 @@
 package com.online.store.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.online.store.entity.CartItem;
+import com.online.store.entity.Product;
 import com.online.store.entity.User;
 
-public interface CartItemRepository extends JpaRepository<CartItemRepository, Long> {
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 	public List<CartItem> findAllByUser(User user);
+
+	public Optional<CartItem> findByUserAndProduct(User user, Product product);
+
+	public boolean existsByUserAndProduct(User user, Product product);
 }
