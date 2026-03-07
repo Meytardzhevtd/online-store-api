6ef415afc5f92052d8ecd2db7f56c3315c88ccb5
Timofey Meytardzhev <meytardzhevtd@yandex.ru>
2026-03-07 17:48:15 +0300

    fix: add new constr

--- src/main/java/com/online/store/exceptions/CartItemNotFoundEexception.java
+++ src/main/java/com/online/store/exceptions/CartItemNotFoundEexception.java
@@ -4,4 +4,8 @@ public class CartItemNotFoundEexception extends RuntimeException {
     public CartItemNotFoundEexception(Long id) {
         super("Cart item with id = " + id + " not found");
     }
+
+    public CartItemNotFoundEexception(Long userId, Long productId) {
+        super("Cart item with userId = " + userId + ", productId = " + productId + " not found");
+    }
 }
