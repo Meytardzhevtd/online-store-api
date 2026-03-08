2663e186727979b30d90f98045665d4e4b3b7a2e
Timofey Meytardzhev <meytardzhevtd@yandex.ru>
2026-03-08 15:34:47 +0300

    add exception

---
+++ src/main/java/com/online/store/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.online.store.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String email) {
+        super("User with email = " + email + " already exists");
+    }
+}
